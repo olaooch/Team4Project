179224eeff921111e25453ac7033ab1c473dd0da
olaooch <fakoredesunday61@gmail.com>
2022-03-07 00:24:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Iphone from "./img/Iphone.jpg";
+import Iphone from "./img/Iphone.png";
 
 
 
@@ -11,35 +11,40 @@ function App (){
     <div className="container">
       
       <div className="textwrapper">
-      <h1 className="text">SOLUTION</h1>
+       <h1 className="text">SOLUTION</h1>
         <div className="textall">
-          <h2>NEED MORE IMFORMATION?</h2>
+
+          <div className="txt1"><h2>NEED MORE IMFORMATION?</h2>
           <h3 className="text2">Ask Our Community</h3>
           <div className="txth5">
           <h5 >01.App user experience & interface</h5>
           <p className="txtp">Lorem ipsum dolor ait amet consectetur adipisicing elit. Non quis voluptetibus saepe<br/>architectto offlciis</p>
           <h5>02.App Performance and Security</h5>
-          <h5>03.Best color Vision & Combination?</h5>
+          <h5>03.Best color Vision & Combination?</h5></div>
           </div>
-        </div>
+
+          <div className="iphonecontainer">
+      
+            <div className="circle"></div> 
+             <img src={Iphone} className="iphone" alt="iphone" />
+              
+          </div>   
+          
+
+        
 
       </div>
-         
-         <div className="iphonecontainer">
-           <div className="iphonewrapper">
-             
-             <img src={Iphone} className="iphone" alt="iphone" />
-             <div className="background"></div>
-             <div className="mainbut">
+      <div className="mainbut">
               <button className="but">Visit Community</button></div> 
              
-           </div>
+         
             
     </div> 
     <div className="container2"> 
               <div className="txtwrapper">
-                <h1>DOWNLOAD</h1>
-              </div>
+               <h1>DOWNLOAD</h1>
+               
+                </div>
               <div className="lastext">
                 <h2>APP AVAILABLE</h2>
                 <h5 className="txth5">Get It Now Free</h5>
